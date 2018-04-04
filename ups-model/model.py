b6d63cbf0d3aa9ea5dba91741bc51aac9bb8da39
Tzu-Ying Chen <wereunique@gmail.com>
2018-04-03 21:10:25 -0400

    Creating python file for modelling

---
+++ ups-model/model.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 21:08:04 2018
+
+@author: Tzu-Ying, Pear, Yi
+Project name: UPS Optimization Model for Network Design
+"""
+
+print("hello WOLRD!")
